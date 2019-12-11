dbbe348fab449502f7f04c092d0c2cbfc4f1f996
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2019-12-11 15:03:46 +0800

    QueueResults

--- Asynchronous programming with async and await/ConsoleApp1/AsyncExampleWPF/MainWindow.xaml.cs
+++ Asynchronous programming with async and await/ConsoleApp1/AsyncExampleWPF/MainWindow.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,17 +24,116 @@ namespace AsyncExampleWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        // *** Declare a System.Threading.CancellationTokenSource.
+        CancellationTokenSource cts;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void startButton_Click(object sender, RoutedEventArgs e)
+        //private void startButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    resultsTextBox.Clear();
+        //    SumPageSizes();
+        //    resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+        //}
+
+        //private async void startButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // Disable the button until the operation is complete.
+        //    startButton.IsEnabled = false;
+
+        //    resultsTextBox.Clear();
+
+        //    // One-step async call.
+        //    await SumPageSizesAsync();
+
+        //    // Two-step async call.
+        //    //Task sumTask = SumPageSizesAsync();
+        //    //await sumTask;
+
+        //    resultsTextBox.Text += "\r\nControl returned to startButton_Click.\r\n";
+
+        //    // Reenable the button in case you want to run the operation again.
+        //    startButton.IsEnabled = true;
+        //}
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
         {
-            resultsTextBox.Clear();
-            SumPageSizes();
-            resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+            // This line is commented out to make the results clearer in the output.
+            //ResultsTextBox.Clear();
+
+            // *** If a download process is already underway, cancel it.
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
+
+            // *** Now set cts to cancel the current process if the button is chosen again.
+            CancellationTokenSource newCTS = new CancellationTokenSource();
+            cts = newCTS;
+
+            try
+            {
+                // ***Send cts.Token to carry the message if there is a cancellation request.
+                await AccessTheWebAsync(cts.Token);
+
+            }
+            // *** Catch cancellations separately.
+            catch (OperationCanceledException)
+            {
+                resultsTextBox.Text += "\r\nDownloads canceled.\r\n";
+            }
+            catch (Exception)
+            {
+                resultsTextBox.Text += "\r\nDownloads failed.\r\n";
+            }
+            // *** When the process is complete, signal that another process can proceed.
+            if (cts == newCTS)
+                cts = null;
         }
+
+
+        // *** Provide a parameter for the CancellationToken from StartButton_Click.
+        private async Task AccessTheWebAsync(CancellationToken ct)
+        {
+            // Declare an HttpClient object.
+            HttpClient client = new HttpClient();
+
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+            var position = 0;
+
+            foreach (var url in urlList)
+            {
+                // *** Use the HttpClient.GetAsync method because it accepts a
+                // cancellation token.
+                HttpResponseMessage response = await client.GetAsync(url, ct);
+
+                // *** Retrieve the website contents from the HttpResponseMessage.
+                byte[] urlContents = await response.Content.ReadAsByteArrayAsync();
+
+                // *** Check for cancellations before displaying information about the
+                // latest site.
+                ct.ThrowIfCancellationRequested();
+
+                DisplayResults(url, urlContents, ++position);
+
+                // Update the total.
+                total += urlContents.Length;
+            }
+
+            // Display the total count for all of the websites.
+            resultsTextBox.Text +=
+                $"\r\n\r\nTOTAL bytes returned:  {total}\r\n";
+        }
+
         private void SumPageSizes()
         {
             // Make a list of web addresses.
@@ -53,7 +154,26 @@ namespace AsyncExampleWPF
             // Display the total count for all of the web addresses.
             resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
         }
+        private async Task SumPageSizesAsync()
+        {
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
 
+            var total = 0;
+            foreach (var url in urlList)
+            {
+                // GetURLContents returns the contents of url as a byte array.
+                byte[] urlContents = await GetURLContentsAsync(url);
+
+                DisplayResults(url, urlContents);
+
+                // Update the total.
+                total += urlContents.Length;
+            }
+
+            // Display the total count for all of the web addresses.
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
         private List<string> SetUpURLList()
         {
             var urls = new List<string>
@@ -97,6 +217,33 @@ namespace AsyncExampleWPF
             return content.ToArray();
         }
 
+
+        private async Task<byte[]> /*byte[]*/ GetURLContentsAsync(string url)
+        {
+            // The downloaded resource ends up in the variable named content.
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for
+            // the response.
+            // Note: you can't use HttpWebRequest.GetResponse in a Windows Store app.
+            //using (WebResponse response = webReq.GetResponse())
+            using (WebResponse response = await webReq.GetResponseAsync())
+            {
+                // Get the data stream that is associated with the specified URL.
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // Read the bytes in responseStream and copy them to content.
+                    //responseStream.CopyTo(content);
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            // Return the result as a byte array.
+            return content.ToArray();
+        }
         private void DisplayResults(string url, byte[] content)
         {
             // Display the length of each website. The string format
@@ -106,6 +253,17 @@ namespace AsyncExampleWPF
             // Strip off the "https://".
             var displayURL = url.Replace("https://", "");
             resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+        private void DisplayResults(string url, byte[] content, int pos)
+        {
+            // Display the length of each website. The string format is designed
+            // to be used with a monospaced font, such as Lucida Console or
+            // Global Monospace.
+
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            // Display position in the URL list, the URL, and the number of bytes.
+            resultsTextBox.Text += $"\n{pos}. {displayURL,-58} {content.Length,8}";
         }
     }
 }
---
+++ Asynchronous programming with async and await/ConsoleApp1/QueueResults/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QueueResults
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        // ***Declare the following variables where all methods can access them.
+        private Task pendingWork = null;
+        private char group = (char)('A' - 1);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ***Verify that each group's results are displayed together, and that
+            // the groups display in order, by marking each group with a letter.
+            group = (char)(group + 1);
+            resultsTextBox.Text += $"\r\n\r\n#Starting group {group}.";
+
+            try
+            {
+                // *** Pass the group value to AccessTheWebAsync.
+                char finishedGroup = await AccessTheWebAsync(group);
+
+                // The following line verifies a successful return from the download and
+                // display procedures.
+                resultsTextBox.Text += $"\r\n\r\n#Group {finishedGroup} is complete.\r\n";
+            }
+            catch (Exception)
+            {
+                resultsTextBox.Text += "\r\nDownloads failed.";
+            }
+        }
+
+        private async Task<char> AccessTheWebAsync(char grp)
+        {
+            HttpClient client = new HttpClient();
+
+            // Make a list of the web addresses to download.
+            List<string> urlList = SetUpURLList();
+
+            // ***Kick off the downloads. The application of ToArray activates all the download tasks.
+            Task<byte[]>[] getContentTasks = urlList.Select(url => client.GetByteArrayAsync(url)).ToArray();
+
+            // ***Call the method that awaits the downloads and displays the results.
+            // Assign the Task that FinishOneGroupAsync returns to the gatekeeper task, pendingWork.
+            pendingWork = FinishOneGroupAsync(urlList, getContentTasks, grp);
+
+            resultsTextBox.Text += $"\r\n#Task assigned for group {grp}. Download tasks are active.\r\n";
+
+            // ***This task is complete when a group has finished downloading and displaying.
+            await pendingWork;
+
+            // You can do other work here or just return.
+            return grp;
+        }
+
+        private async Task FinishOneGroupAsync(List<string> urls, Task<byte[]>[] contentTasks, char grp)
+        {
+            // ***Wait for the previous group to finish displaying results.
+            if (pendingWork != null) await pendingWork;
+
+            int total = 0;
+
+            // contentTasks is the array of Tasks that was created in AccessTheWebAsync.
+            for (int i = 0; i < contentTasks.Length; i++)
+            {
+                // Await the download of a particular URL, and then display the URL and
+                // its length.
+                byte[] content = await contentTasks[i];
+                DisplayResults(urls[i], content, i, grp);
+                total += content.Length;
+            }
+
+            // Display the total count for all of the websites.
+           resultsTextBox.Text +=
+                $"\r\n\r\nTOTAL bytes returned:  {total}\r\n";
+        }
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com/library/hh191443.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/jj155761.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/hh524395.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+        // ***Add a parameter for the group label.
+        private void DisplayResults(string url, byte[] content, int pos, char grp)
+        {
+            // Display the length of a website. The string format is designed to be 
+            // used with a monospaced font, such as Lucida Console or Global Monospace.
+
+            // Strip off the "http://".
+            var displayURL = url.Replace("http://", "");
+            // Display position in the URL list, the URL, and the number of bytes.
+           resultsTextBox.Text += string.Format("\r\n{0}-{1}. {2,-58} {3,8}", grp, pos + 1, displayURL, content.Length);
+        }
+    }
+}
