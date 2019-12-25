4b33d6e2b93cc835e5f93347dd07bee496103f68
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2019-12-25 17:04:35 +0800

    将视图添加到 ASP.NET Core MVC 应用

--- 使用 ASP.NET Core MVC 创建 Web 应用/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ 使用 ASP.NET Core MVC 创建 Web 应用/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -9,10 +9,14 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
+        //// GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         //// GET: /HelloWorld/Welcome/ 
@@ -28,9 +32,17 @@ namespace MvcMovie.Controllers
         //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         //}
 
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
