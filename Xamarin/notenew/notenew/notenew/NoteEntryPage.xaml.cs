e99743406f76ce5527bf920c353e4a6a2e1e757a
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2020-01-15 17:19:14 +0800

    将数据存储在本地 SQLite.NET 数据库中 ok

---
+++ Xamarin/notenew/notenew/notenew/NoteEntryPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using notenew.Models;
+using System;
+using Xamarin.Forms;
+namespace notenew
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
