b44ad9ba4543b787fa36792587c67c6d531b5dc5
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2020-01-15 15:54:24 +0800

    在多页 Xamarin 窗体应用程序中执行导航

---
+++ Xamarin/Notes/Notes/Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Notes.Models;
+ 
+namespace Notes
+{ 
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender,EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.FileName))
+            {
+                //Save
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                //Update
+                File.WriteAllText(note.FileName, note.Text);
+            }
+
+            await Navigation.PopAsync();
+
+        }
+
+        async void OnDeleteButtonClicked(object sender,EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.FileName))
+            {
+                File.Delete(note.FileName);
+            }
+
+            await Navigation.PopAsync();
+
+        }
+    }
+}
